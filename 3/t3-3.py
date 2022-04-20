4e2d72873238ffba2b218866678a4b6f16074eb5
lirpa <thelastrist@gmail.com>
2022-04-20 16:33:23 +0300

    stopped at lesson 3-4, ex.2

---
+++ 3/t3-3.py
@@ -0,0 +1,16 @@
+# ex 1
+
+a, b, c = 8, 5, 3
+print(((a / (b + c)) - a) / c)
+print()
+
+# ex 2
+bus = 5
+metro = 3
+
+result = (6 + (10 - bus) ** 2) / (metro + 1) + 132 / (2 + bus)
+print(result)
+
+# ex 3
+ex3_var = ( (-3 + (8 ** 2 - 12) * 4 ** (3 ** 2)) ) / (2 * 18)
+print(ex3_var)
---
+++ 3/t3-4.py
@@ -0,0 +1,13 @@
+# ex 1
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum = ", a + b)
+
+# ex 2
+c = int(input("Enter third number: "))
+d = int(input("Enter fourth number: "))
+result = 2 * (c + 5 + a * b / 4 * b) * (d - 2 * a ** 3 / 30) - 10
+print("Answer is:", result)
+
+# ex 3
