6c6dd54ef9f59b9bf6857ed28bb09a2111837775
lirpa <thelastrist@gmail.com>
2022-04-20 15:45:04 +0300

    lesson 3-2 finished

---
+++ 3/t3-2.py
@@ -0,0 +1,15 @@
+﻿print(6 * 39)
+print(3 ** 5)
+print(-8 / -4)
+print(10 / 2 + 6)
+
+var1, var2, var3 = 5, 4, 2
+print("Сложение:", var1 + var2 + var3)
+print("Вычитание:", var1 - var2 - var3)
+print("Умножение:", var1 * var2 * var3)
+print("Деление:", var1 / var2 / var3)
+print("Возведение в степень:", var1 ** var2 ** var3)
+
+dividend, divider = 65, 0
+print("Quotient is:", dividend / divider)
+
