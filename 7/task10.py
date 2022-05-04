f3814c993b2e0385320ee7551ef7efbb85d9e3bc
lirpa <thelastrist@gmail.com>
2022-05-04 06:43:09 +0300

    less7 finished, sending to check

--- 7/task10.py
+++ 7/task10.py
@@ -8,3 +8,16 @@ print('Задача 10.')
 # далее еще N − 1 чисел: 
 # номера оставшихся карточек (различные числа от 1 до N).
 # Программа должна вывести номер потерянной карточки.
+
+cards_quantity = int(input("Введите количество карточек: "))
+#cards_quantity += 1 # for range
+sum, sum_all = 0, cards_quantity
+
+for counter in range(1, cards_quantity):
+    user_number = int(input("Введите число №"+str(counter)+":"))
+    sum += user_number
+    sum_all += counter
+
+
+
+print("Пропала карточка №", sum_all - sum)
