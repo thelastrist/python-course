45e0ca6e9b3ea81224da24a769b86c9cc81e88d0
lirpa <afell@mail.ru>
2022-05-14 19:37:40 +0300

    task 10 ready

--- 8/task10.py
+++ 8/task10.py
@@ -37,33 +37,44 @@ print('Задача 10. Кинотеатр')
 i, counter, diff, max, min = 0, 0, 0, 0, 0
 b_boys = True
 out_string = ''
-boys_X = int(input("Enter quantity of boys (X): "))
-girls_Y = int(input("Enter girls quantity (Y): "))
+#boys_X = int(input("Enter quantity of boys (X): "))
+#girls_Y = int(input("Enter girls quantity (Y): "))
 
-if boys_X // girls_Y > 2 or girls_Y // boys_X > 2:
-    print("No solution")
-    exit(0)
+def draw_line(boys_X, girls_Y):
+    out_string = ''
+    if boys_X / girls_Y > 2 or girls_Y / boys_X > 2:
+#        print("No solution")
+#        exit(0)
+        return("No Solution")
 
-if boys_X > girls_Y:
-    max = boys_X
-    min = girls_Y
-    b_boys = True
-else:
-    max = girls_Y
-    min = boys_X
-    b_boys = False
-
-diff = max - min
-for i in range(1, min + 1):
-    if b_boys:
-        out_string += 'BG'
-        if diff > 0:
-            out_string += 'B'
-            diff -= 1
+    if boys_X > girls_Y:
+        max = boys_X
+        min = girls_Y
+        b_boys = True
     else:
-        out_string += 'GB'
-        if diff > 0:
-            out_string += 'G'
-            diff -= 1
+        max = girls_Y
+        min = boys_X
+        b_boys = False
 
-print(out_string)
+    diff = max - min
+    for i in range(1, min + 1):
+        if b_boys:
+            out_string += 'BG'
+            if diff > 0:
+                out_string += 'B'
+                diff -= 1
+        else:
+            out_string += 'GB'
+            if diff > 0:
+                out_string += 'G'
+                diff -= 1
+    return out_string
+
+boys_X = 10 # int(input("Enter quantity of boys (X): "))
+girls_Y = 10 #int(input("Enter girls quantity (Y): "))
+for i in range(1, boys_X):
+    for j in range(1, girls_Y):
+        out_string=draw_line(i, j)
+        print(i, j, out_string)
+
+#print(boys_X, girls_Y, out_string)
